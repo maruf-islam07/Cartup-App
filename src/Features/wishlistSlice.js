8c104c1a630a9bee4e7cd6aaec85be3b3d826da1
maruf-islam07 <mdmaroofislam2004@gmail.com>
2026-04-06 23:25:51 +0600

    add wishlist in our project

--- src/Features/wishlistSlice.js
+++ src/Features/wishlistSlice.js
@@ -1,15 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState ={
-    items : [];
-}
+const initialState = {
+  items: localStorage.getItem("wishlist")
+    ? JSON.parse(localStorage.getItem("wishlist"))
+    : [],
+};
 
 const wishlistSlice = createSlice({
-    name: wishlist,
-    initialState,
-    reducers:{
-        addToWishlist: (state, action)={
-            
-        }
-    }
-})
+  name: wishlist,
+  initialState,
+  reducers: {
+    addToWishlist: (state, action) => {
+      const item = action.payload;
+      const exists = state.wishlist.find((i) => i.id === item.id);
+
+      if (!exists) {
+        state.wishlist.push(item);
+      }
+      localStorage.setItem("wishlist", JSON.stringify(state.wishlist));
+    },
+    removeFromWishlist: (state, action) => {
+      state.wishlist = state.wishlist.filter(
+        (item) => item.id !== action.payload,
+      );
+    },
+  },
+});
